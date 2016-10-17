30b486fe9b168fd4dddd1fa3347b96d11d2f102e
Marcin Dawidowski <mdawidowski@sigma.ug.edu.pl>
2016-10-17 21:47:20 +0200

    Little problems with decrypting

--- Zad1/Zad1.c
+++ Zad1/Zad1.c
@@ -26,8 +26,38 @@ char szyfr(char znak, int key){
       return znak;
       }
       if(znak!=a[x]){
-        znak = NULL;
+        break;
+      }
+    }
+  }
+}
+
+char deszyfr(char znak, int key){
+  int x,sum,z;
+  char a[25]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'};
+  char d[25]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z'};
+
+  for(x=0; x<=25; x++){
+    if (znak >= 'A' && znak <= 'Z'){
+      for(z=0; z<=25; z++){
+        a[z]=d[z];
+      }
+    }
+    if(znak == '\n'){
+      znak = '\n';
+      return znak;
+    }
+    if(znak==a[x]){
+      sum=x-key;
+      if(sum<0){
+        znak=a[sum+25];
         return znak;
+      } else if(sum>0 && sum<26){
+      znak=a[sum];
+      return znak;
+      }
+      if(znak!=a[x]){
+        break;
       }
     }
   }
@@ -41,18 +71,15 @@ int main(){
    printf ("Nie mogę otworzyć pliku crypto.txt do zapisu!\n");
    exit(1);
    }
-   //dotyczy pliku z tekstem odszyfrowanym
-   FILE *decrypt;
-   if((decrypt=fopen("decrypt.txt", "w"))==NULL) {
-    printf ("Nie mogę otworzyć pliku decrypt.txt do zapisu!\n");
-    exit(1);
-    }
   //dotyczy klucza
   int kluczyk;
   FILE *fkey;
   fkey=fopen("key.txt","r");
   fscanf(fkey,"%d",&kluczyk);
-  fclose(fkey);
+  if(kluczyk<-26 || kluczyk>26){
+    printf("Błędny klucz. Do poprawy\n");
+    exit(1);
+  }
   //dotyczy pliku z tekstem orygnialnym
   FILE *plain;
   plain=fopen("plain.txt","r");
@@ -61,11 +88,28 @@ int main(){
     if(feof(plain)){
       break;
     }
-    printf("%c", znak);
     char wynik = szyfr(znak, kluczyk);
     fprintf (crypto, "%c", wynik);
   }
-  fclose (crypto);
+  fclose(crypto);
+  //dotyczy pliku z tekstem odszyfrowanym
+  FILE *decrypt;
+  if((decrypt=fopen("decrypt.txt", "w"))==NULL) {
+   printf ("Nie mogę otworzyć pliku decrypt.txt do zapisu!\n");
+   exit(1);
+   }
+   crypto=fopen("crypto.txt","r");
+   while(crypto != NULL){
+     znak = fgetc(crypto);
+     if(feof(crypto)){
+       break;
+     }
+     char wynik2 = deszyfr(znak, kluczyk);
+     fprintf (decrypt, "%c", wynik2);
+   }
+  fclose(decrypt);
+  fclose(crypto);
   fclose(plain);
+  fclose(fkey);
   return 0;
 }
