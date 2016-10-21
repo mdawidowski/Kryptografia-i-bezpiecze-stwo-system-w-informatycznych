f6b4e5f251da6091cf22ffd06c06e8f79d3ff6d9
Marcin Dawidowski <mdawidowski@sigma.ug.edu.pl>
2016-10-21 12:52:07 +0200

    Update Zad1.c

--- Zad1/Zad1.c
+++ Zad1/Zad1.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//////////////////////////////////////////////////////////////
-//////////////////////funkcje odczytujące/////////////////////
-//////////////////////////////////////////////////////////////
-// char odczytzaszyfrowany(){
-//   FILE *crypto;
-//   if((crypto=fopen("crypto.txt", "w"))==NULL) {
-//    printf ("Nie mogę otworzyć pliku crypto.txt do zapisu!\n");
-//    exit(1);
-//    }
-//   while(crypto != NULL){
-//    znak = fgetc(crypto);
-//     if(feof(crypto)){
-//      break;
-//    }
-//  }
-// }
-//////////////////////////////////////////////////////////////
-///////////////////////funkcje szyfrujące/////////////////////
-//////////////////////////////////////////////////////////////
+
 int szyfrujcezar(char znak, int key){
   int x,sum,z;
   char a[25]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'};
@@ -34,12 +16,16 @@ int szyfrujcezar(char znak, int key){
       znak = '\n';
       return znak;
     }
+    if(znak == ' '){
+      znak = ' ';
+      return znak;
+    }
     if(znak==a[x]){
       sum=x+key;
       if(sum>25){
         znak=a[sum-25];
         return znak;
-      } else if(sum<26){
+      } else if(sum<=25){
       znak=a[sum];
       return znak;
       }
@@ -49,42 +35,42 @@ int szyfrujcezar(char znak, int key){
     }
   }
 }
-// int odszyfrujcezar(char znak, int key){
-//   int x,sum,z;
-//   char a[25]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'};
-//   char d[25]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z'};
-//
-//   for(x=0; x<=25; x++){
-//     if (znak >= 'A' && znak <= 'Z'){
-//       for(z=0; z<=25; z++){
-//         a[z]=d[z];
-//       }
-//     }
-//     if(znak == '\n'){
-//       znak = '\n';
-//       fprintf (decrypt, "%c", znak);
-//     }
-//     if(znak==a[x]){
-//       sum=x-key;
-//       if(sum<0){
-//         znak=a[sum+25];
-//         fprintf (decrypt, "%c", znak);
-//       } else if(sum>0 && sum<26){
-//         znak=a[sum];
-//         fprintf (decrypt, "%c", znak);
-//       }
-//     }
-//   }
-// }
+int odszyfrujcezar(char znak, int key){
+  int x,sum,z;
+  char a[25]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'};
+  char d[25]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z'};
+
+  for(x=0; x<=25; x++){
+    if (znak >= 'A' && znak <= 'Z'){
+      for(z=0; z<=25; z++){
+        a[z]=d[z];
+      }
+    }
+    if(znak == '\n'){
+      znak = '\n';
+      return znak;
+    }
+    if(znak==a[x]){
+      sum=x-key;
+      if(sum<0){
+        znak=a[sum+25];
+        return znak;
+      } else if(sum>25){
+          znak=a[sum-25];
+      } else if(sum>0 && sum<26){
+          znak=a[sum];
+          return znak;
+      }
+    }
+  }
+}
 //////////////////////////////////////////////////////////////
 ///////////////////////funkcje wykonujące/////////////////////
 //////////////////////////////////////////////////////////////
 int wykonuj(int parametr){
   int kluczyk, kluczyk1, kluczyk2;
   char znak,wynik;
-  FILE *fkey;
-  FILE *plain;
-  FILE *crypto;
+  FILE *fkey,*plain,*crypto,*decrypt;
 
   switch (parametr) {
 
@@ -112,7 +98,27 @@ int wykonuj(int parametr){
         fprintf (crypto, "%c", wynik);
     }
 
-    // case 2:     //odszyfruj Cezar
+     case 2:     //odszyfruj Cezar
+       //odczyt klucza
+       fkey=fopen("key.txt","r");
+       fscanf(fkey,"%d",&kluczyk);
+       if(kluczyk<-25 || kluczyk>25){
+         printf("Błędny klucz. Do poprawy\n");
+         exit(1);
+       }
+       if((decrypt=fopen("decrypt.txt", "w"))==NULL) {
+        printf ("Nie mogę otworzyć pliku decrypt.txt do zapisu!\n");
+        exit(1);
+        }
+        crypto=fopen("crypto.txt","r");
+        while(crypto != NULL){
+          znak = fgetc(crypto);
+          if(feof(crypto)){
+            break;
+          }
+          char wynik2 = odszyfrujcezar(znak, kluczyk);
+          fprintf (decrypt, "%c", wynik2);
+        }
     // case 3:     //kryptoanaliza z jawnym Cezar
     // case 4:     //kryptoanaliza w oparciu o kryptogram Cezar
 
@@ -120,8 +126,8 @@ int wykonuj(int parametr){
       //odczyt kluczy
       fkey=fopen("key.txt","r");
       fscanf(fkey,"%d%d",&kluczyk1, &kluczyk2);
-      if(kluczyk1<-25 || kluczyk1>25){
-        printf("Błędny klucz. Do poprawy\n");
+      if(kluczyk1<-25 || kluczyk1>25 || kluczyk2<-25 || kluczyk2>25){
+        printf("Błąd w pliku z kluczami. Do poprawy\n");
         exit(1);
       }
       //odczyt tekstu jawnego
