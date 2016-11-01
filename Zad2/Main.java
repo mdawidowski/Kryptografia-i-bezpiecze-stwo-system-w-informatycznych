a7e2a96b20f3bef167e7dc188a5583cd84e1fc5e
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-11-01 19:47:47 +0100

    updated repository

--- Zad2/Main.java
+++ Zad2/Main.java
@@ -1,7 +1,48 @@
 import java.io.*;
 import java.util.*;
+
 public class Main{
 
+  public byte[] kluczdoascii(){
+    String klucz = new String("");
+    File fkey = new File("key.txt");
+    try {
+      RandomAccessFile key = new RandomAccessFile(fkey, "r");
+    try {
+      klucz = key.readLine();
+      byte[] bytes = klucz.getBytes("US-ASCII");
+      for(int i=0; i<=klucz.length()-1; i++){
+        bytes[i] -= 97;
+      }
+      return bytes;
+    } catch (IOException e) { System.out.println("Problem we/wy"); }
+    } catch (FileNotFoundException e) { System.out.println("Brak pliku z kluczem");  }
+    return null;
+  }
+
+  public byte[] tekstdoascii(){
+    String tekst = new String("");
+    String out = new String("");
+    File plain = new File("plain.txt");
+    StringBuilder build = new StringBuilder(tekst);
+    try {
+      RandomAccessFile input = new RandomAccessFile(plain, "r");
+      while (tekst != null) {
+       try {
+         tekst = input.readLine();
+         if (tekst != null) {
+           build.append(tekst);
+          }
+       } catch (IOException e) { System.out.println("Błąd we/wy"); }
+      }
+      try {
+        out = build.toString();
+        byte[] bytes = out.getBytes("US-ASCII");
+        return bytes;
+      } catch (UnsupportedEncodingException e) { System.out.println("Problem z zamianą na ASCII"); }
+    } catch (FileNotFoundException e) { System.out.println("Błąd we/wy"); }
+    return null;
+  }
   public void przygotuj(){
     String tekst = new String("");
     File orig = new File("orig.txt");
@@ -10,14 +51,17 @@ public class Main{
       RandomAccessFile input = new RandomAccessFile(orig, "r");
       RandomAccessFile output = new RandomAccessFile(plain, "rw");
       try {
-        tekst = "";
         while (tekst != null){
           tekst = input.readLine();
           if (tekst != null) {
             tekst = tekst.replaceAll("\\W", "");
+            tekst = tekst.replaceAll("\\d", "");
+            tekst = tekst.toLowerCase();
             output.writeBytes(tekst + "\n");
           }
         }
+        input.close();
+        output.close();
       } catch (IOException e) {
         System.out.println("Problem we/wy");
       }
@@ -25,11 +69,26 @@ public class Main{
     catch (FileNotFoundException e) {
       System.out.println("Brak pliku");
     }
-  //  tekst = input.readLine();
   }
 
-  public void szyfruj(){
-    System.out.println("Funkcja niegotowa");
+  public void szyfruj(byte[] asciiklucz, byte[] asciitekst){
+    int pom, z=0;
+    char[] szyfr = new char[asciitekst.length];
+    String tekst = new String("");
+    File crypto = new File("crypto.txt");
+    try {
+      RandomAccessFile output = new RandomAccessFile(crypto, "rw");
+        for(int x=0;x<asciitekst.length;x++){
+          if(z>=asciiklucz.length){ z = 0; }
+          pom = asciitekst[x] + asciiklucz[z];
+          if(pom>122){ pom -= 26; }
+          szyfr[x] = (char)pom;
+          try {
+            output.writeChar(szyfr[x]);
+          } catch (IOException e) { System.out.println("Błąd we/wy"); }
+          z += 1;
+        }
+    } catch (FileNotFoundException e) { System.out.println("Brakuj pliku crypto!"); }
   }
 
   public void deszyfruj(){
@@ -48,7 +107,9 @@ public class Main{
       start.przygotuj();
       break;
       case "-e":
-      start.szyfruj();
+      byte[] asciiklucz = start.kluczdoascii();
+      byte[] asciitekst = start.tekstdoascii();
+      start.szyfruj(asciiklucz, asciitekst);
       break;
       case "-d":
       start.deszyfruj();
