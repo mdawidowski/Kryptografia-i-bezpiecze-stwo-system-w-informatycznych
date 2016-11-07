8a13f7841ba006885290b109d8b1b5e4ab09d1df
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-11-07 15:23:34 +0100

    szyfrowanie dziala w 100 procentach

--- Zad3/Main.java
+++ Zad3/Main.java
@@ -2,6 +2,70 @@ import java.io.*;
 
 public class Main{
 
+  public void kryptoanaliza(){
+    File crypto = new File("crypto.txt");
+    File decrypt = new File("decrypt.txt");
+    String tekst = "";
+    try {
+      try {
+        try {
+          Reader reader = new InputStreamReader(new FileInputStream(crypto),"US-ASCII");
+          BufferedReader fin = new BufferedReader(reader);
+          Writer writer = new OutputStreamWriter(new FileOutputStream(decrypt), "US-ASCII");
+          BufferedWriter fout = new BufferedWriter(writer);
+          tekst = fin.readLine();
+          while (tekst!=null) {
+            System.out.println(tekst);
+            tekst = fin.readLine();
+          }
+        } catch (UnsupportedEncodingException e) {}
+      } catch (FileNotFoundException e) {}
+    } catch (IOException e) {}
+  }
+
+  public void szyfruj(byte[] klucz){
+   File plain = new File("plain.txt");
+   File crypto = new File("crypto.txt");
+   String tekst="", all = "";
+   StringBuilder sB = new StringBuilder(tekst);
+   int wynik = 0;
+   try {
+     try {
+       try {
+         Reader reader = new InputStreamReader(new FileInputStream(plain),"US-ASCII");
+         BufferedReader fin = new BufferedReader(reader);
+         Writer writer = new OutputStreamWriter(new FileOutputStream(crypto), "US-ASCII");
+         BufferedWriter fout = new BufferedWriter(writer);
+         tekst = fin.readLine();
+         while (tekst!=null) {
+           sB.append(tekst);
+           sB.append("\n");
+           tekst = fin.readLine();
+         }
+         all = sB.toString();
+         byte[] bytes = all.getBytes("US-ASCII");
+         int z = 0, i= 0;
+         char[] znak = new char[bytes.length];
+         StringBuilder record = new StringBuilder(znak[i]);
+         for (i=0;i<=bytes.length-1;i++ ) {
+           if(z>=klucz.length-1){ z = 0; }
+           wynik = bytes[i] + klucz[z];
+           z += 1;
+           if(wynik>122){ wynik-=25; }
+           if(bytes[i]==10){ wynik = 10; z = 0; }
+           znak[i] = (char)wynik;
+        //   System.out.println("klucz: " + klucz[z] + " litera: " + bytes[i] + " = " + wynik + " = " + znak[i]);
+           record.append(znak[i]);
+         }
+         all = record.toString();
+         fout.write(all);
+         fout.close();
+         fin.close();
+       } catch (UnsupportedEncodingException e) {}
+     } catch (FileNotFoundException e) {}
+   } catch (IOException e) {}
+ }
+
   public byte[] kluczdoascii(){
    String klucz = new String("");
    File fkey = new File("key.txt");
@@ -20,49 +84,6 @@ public class Main{
    } catch (FileNotFoundException e) { System.out.println("Brak pliku z kluczem");  }
    } catch (IOException e) { System.out.println("Problem we/wy"); }
    return null;
- }
-
- public void szyfruj(byte[] klucz){
-   File plain = new File("plain.txt");
-   File crypto = new File("crypto.txt");
-   String tekst="", all = "";
-   StringBuilder sB = new StringBuilder(tekst);
-   int wynik = 0;
-   try {
-     try {
-       try {
-         Reader reader = new InputStreamReader(new FileInputStream(plain),"ASCII");
-         BufferedReader fin = new BufferedReader(reader);
-         Writer writer = new OutputStreamWriter(new FileOutputStream(crypto), "UTF-8");
-         BufferedWriter fout = new BufferedWriter(writer);
-         tekst = fin.readLine();
-         while (tekst!=null) {
-           sB.append(tekst);
-           sB.append("\n");
-           tekst = fin.readLine();
-         }
-         all = sB.toString();
-         byte[] bytes = all.getBytes("US-ASCII");
-         int z = 0, i= 0;
-         char[] znak = new char[bytes.length];
-         StringBuilder record = new StringBuilder(znak[i]);
-         for (i=0;i<=bytes.length-1;i++ ) {
-           if(z>=klucz.length-1){ z = 0; }
-           wynik = bytes[i] + klucz[z];
-           z += 1;
-           if(wynik>122){ wynik-=25; }
-           if(bytes[i]==10){ wynik = 10; }
-           znak[i] = (char)wynik;
-        //   System.out.println("klucz: " + klucz[z] + " litera: " + bytes[i] + " = " + wynik + " = " + znak[i]);
-           record.append(znak[i]);
-         }
-         all = record.toString();
-         fout.write(all);
-         fout.close();
-         fin.close();
-       } catch (UnsupportedEncodingException e) {}
-     } catch (FileNotFoundException e) {}
-   } catch (IOException e) {}
  }
 
   public void przygotuj(){
@@ -99,16 +120,18 @@ public class Main{
     byte[] klucz;
     Main start = new Main();
     switch (x[0]) {
+
       case "-p":
       start.przygotuj();
       break;
+
       case "-e":
       klucz = start.kluczdoascii();
       start.szyfruj(klucz);
-      //szyfrowanie
-
       break;
+
       case "-k":
+      start.kryptoanaliza();
       //kryptoanaliza
     }
   }
