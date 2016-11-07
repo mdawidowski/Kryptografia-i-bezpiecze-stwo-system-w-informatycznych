1df236b439d68731287a2e35c5067df4bab18c76
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-11-07 12:50:36 +0100

    updated repository

--- Zad3/Main.java
+++ Zad3/Main.java
@@ -5,11 +5,28 @@ public class Main{
   public void przygotuj(){
     File orig = new File("orig.txt");
     File plain = new File("plain.txt");
+    String tekst = "", all = "";
+    StringBuilder sB = new StringBuilder(all);
     try {
       try {
         try {
-          Reader reader = new InputStreamReader(new FileInputStream(orig),"UTF-8");
+          Reader reader = new InputStreamReader(new FileInputStream(orig),"ASCII");
           BufferedReader fin = new BufferedReader(reader);
+          Writer writer = new OutputStreamWriter(new FileOutputStream(plain), "UTF-8");
+          BufferedWriter fout = new BufferedWriter(writer);
+          tekst = fin.readLine();
+          while(tekst!=null){
+            tekst = tekst.replaceAll("[,.!?:;'-0123456789]", "");
+            tekst = tekst.toLowerCase();
+            System.out.println(tekst);
+            sB.append(tekst);
+            sB.append("\n");
+            tekst = fin.readLine();
+          }
+          all = sB.toString();
+          fout.write(all);
+          fin.close();
+          fout.close();
         } catch (UnsupportedEncodingException e) {}
       } catch (FileNotFoundException e) {}
     } catch (IOException e) {}
@@ -20,7 +37,7 @@ public class Main{
     Main start = new Main();
     switch (x[0]) {
       case "-p":
-      //przygotowywanie pliku
+      start.przygotuj();
       break;
       case "-e":
       //szyfrowanie
