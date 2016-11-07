122e786d1747632466cd7dd3e16085bb39add439
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-11-07 14:49:34 +0100

    updated repository

--- Zad3/Main.java
+++ Zad3/Main.java
@@ -22,6 +22,49 @@ public class Main{
    return null;
  }
 
+ public void szyfruj(byte[] klucz){
+   File plain = new File("plain.txt");
+   File crypto = new File("crypto.txt");
+   String tekst="", all = "";
+   StringBuilder sB = new StringBuilder(tekst);
+   int wynik = 0;
+   try {
+     try {
+       try {
+         Reader reader = new InputStreamReader(new FileInputStream(plain),"ASCII");
+         BufferedReader fin = new BufferedReader(reader);
+         Writer writer = new OutputStreamWriter(new FileOutputStream(crypto), "UTF-8");
+         BufferedWriter fout = new BufferedWriter(writer);
+         tekst = fin.readLine();
+         while (tekst!=null) {
+           sB.append(tekst);
+           sB.append("\n");
+           tekst = fin.readLine();
+         }
+         all = sB.toString();
+         byte[] bytes = all.getBytes("US-ASCII");
+         int z = 0, i= 0;
+         char[] znak = new char[bytes.length];
+         StringBuilder record = new StringBuilder(znak[i]);
+         for (i=0;i<=bytes.length-1;i++ ) {
+           if(z>=klucz.length-1){ z = 0; }
+           wynik = bytes[i] + klucz[z];
+           z += 1;
+           if(wynik>122){ wynik-=25; }
+           if(bytes[i]==10){ wynik = 10; }
+           znak[i] = (char)wynik;
+        //   System.out.println("klucz: " + klucz[z] + " litera: " + bytes[i] + " = " + wynik + " = " + znak[i]);
+           record.append(znak[i]);
+         }
+         all = record.toString();
+         fout.write(all);
+         fout.close();
+         fin.close();
+       } catch (UnsupportedEncodingException e) {}
+     } catch (FileNotFoundException e) {}
+   } catch (IOException e) {}
+ }
+
   public void przygotuj(){
     File orig = new File("orig.txt");
     File plain = new File("plain.txt");
@@ -38,7 +81,6 @@ public class Main{
           while(tekst!=null){
             tekst = tekst.replaceAll("[,.!?:;'-0123456789]", "");
             tekst = tekst.toLowerCase();
-            System.out.println(tekst);
             sB.append(tekst);
             sB.append("\n");
             tekst = fin.readLine();
@@ -62,9 +104,7 @@ public class Main{
       break;
       case "-e":
       klucz = start.kluczdoascii();
-      for(int i=0;i<=klucz.length-1;i++){
-        System.out.println(klucz[i]);
-      }
+      start.szyfruj(klucz);
       //szyfrowanie
 
       break;
