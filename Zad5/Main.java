05e78b2c34fc4596706fa434d6d8e7750cb665a5
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-12-03 20:42:29 +0100

    Zad 5 obsługa plików

--- Zad5/Main.java
+++ Zad5/Main.java
@@ -1,8 +1,25 @@
-import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class Main{
-  public static void main(String[] args) {
-    Image img;
-    img = getImage("obraz.jpg");
+  public static void main(String args[])throws IOException{
+    BufferedImage image = null;
+    File f = null;
+
+    try{
+      f = new File("obraz.jpg");
+      image = ImageIO.read(f);
+    }catch(IOException e){
+      System.out.println("Error: "+e);
+    }
+
+    try{
+      f = new File("obraz1.jpg");
+      ImageIO.write(image, "jpg", f);
+    }catch(IOException e){
+      System.out.println("Error: "+e);
+    }
   }
 }
