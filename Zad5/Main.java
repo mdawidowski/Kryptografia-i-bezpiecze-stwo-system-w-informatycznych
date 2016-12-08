0ac35ea81fbae1a3f5aba4c2680f90499f59f320
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-12-08 22:30:24 +0100

    postepy Zad5

--- Zad5/Main.java
+++ Zad5/Main.java
@@ -9,15 +9,22 @@ public class Main{
     File f = null;
 
     try{
-      f = new File("obraz.jpg");
+      f = new File("plain.bmp");
       image = ImageIO.read(f);
+      int x = image.getWidth();
+      int y = image.getHeight();
+      System.out.println(x + " " + y);
+      int color = image.getRGB(500, 500);
+      color += 2;
+      image.setRGB(0, 0, color);
+      System.out.println(color);
     }catch(IOException e){
       System.out.println("Error: "+e);
     }
 
     try{
-      f = new File("obraz1.jpg");
-      ImageIO.write(image, "jpg", f);
+      f = new File("obraz1.bmp");
+      ImageIO.write(image, "bmp", f);
     }catch(IOException e){
       System.out.println("Error: "+e);
     }
