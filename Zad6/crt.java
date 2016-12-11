4d491129b1aa6fc1a783236f01f25c6ec0b6414b
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-12-11 22:36:56 +0100

    duże postępy Zad6

--- Zad6/crt.java
+++ Zad6/crt.java
@@ -1,30 +1,55 @@
 import java.io.*;
 
 public class crt{
-  public static void main(String[] args) throws IOException,UnsupportedEncodingException, FileNotFoundException {
-  File odczyt = new File("uklad.txt");
-  File zapis = new File("crt.txt");
-  Reader reader = new InputStreamReader(new FileInputStream(odczyt),"US-ASCII");
-  BufferedReader fin = new BufferedReader(reader);
-  Writer writer = new OutputStreamWriter(new FileOutputStream(zapis), "US-ASCII");
-  BufferedWriter fout = new BufferedWriter(writer);
-  String tekst = fin.readLine(),a="",m="";
-  int[] p = new int[20];
-  int[] b = new int[20];
-  int x=0;
-  while (tekst!=null) {
-    String[] part = tekst.split(" ");
-    a = part[0];
-    m = part[1];
-    if (a.matches("[0-9]+") && a.matches("[0-9]+")) {
-      p[x] = Integer.parseInt(a);
-      b[x] = Integer.parseInt(m);
-    }else {
-      System.out.println("Znaleziono znak, który nie jest cyfrą!");
-      System.exit(1);
+  public static int NWD(int a,int b){
+    int c;
+    while(b != 0)
+    {
+        c = a % b;
+        a = b;
+        b = c;
     }
-    x++;
-    tekst = fin.readLine();
-  }
+    return a;
+}
+
+  public static void main(String[] args) throws IOException,UnsupportedEncodingException, FileNotFoundException {
+    File odczyt = new File("uklad.txt");
+    File zapis = new File("crt.txt");
+    Reader reader = new InputStreamReader(new FileInputStream(odczyt),"US-ASCII");
+    BufferedReader fin = new BufferedReader(reader);
+    Writer writer = new OutputStreamWriter(new FileOutputStream(zapis), "US-ASCII");
+    BufferedWriter fout = new BufferedWriter(writer);
+    String tekst = fin.readLine(),a="",m="";
+    int[] p = new int[20];
+    int[] b = new int[20];
+    int x=0,mzero,azero;
+    while (tekst!=null) {
+      String[] part = tekst.split(" ");
+      a = part[0];
+      m = part[1];
+      if (a.matches("[0-9]+") && a.matches("[0-9]+")) {
+        p[x] = Integer.parseInt(a);
+        b[x] = Integer.parseInt(m);
+      }else {
+        System.out.println("Znaleziono znak, który nie jest cyfrą!");
+        System.exit(1);
+      }
+      tekst = fin.readLine();
+      int check = NWD(p[x],b[x]);
+      if (check != 1) {
+        System.out.println("Cyfry nie są względnie pierwsze parami");
+      }
+      x++;
+    }
+    mzero = 1;
+    azero = 0;
+    for (int z=0; z<x; z++) {
+      mzero *= b[z];
+      azero += p[z] % b[z];
+    }
+    System.out.println(azero + " " + mzero);
+    fin.close();
+    fout.write(mzero + " " + azero);
+    fout.close();
   }
 }
